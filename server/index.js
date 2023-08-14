db606bbc6ee512e0a0d5c86f9973d2b6a498ec20
Rjchi <richardospina18@gmail.com>
2023-08-14 17:30:28 -0500

    crear conexion a la base de datos

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const PORT = require("./config.js");
+// Ruta
+const indexRoutes = require("./routes/index.routes.js")
 
 const app = express();
+
+app.use(indexRoutes)
+
 
 app.listen(PORT, () => console.log(`PORT ${PORT}`))
