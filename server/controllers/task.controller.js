8beb3bcb96afea35be89c6028cc9b968d57280fb
Rjchi <richardospina18@gmail.com>
2023-08-14 18:26:25 -0500

    utilizar metodo delete para  eliminar una tarea

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -36,8 +36,15 @@ const updateTask = (req, res) => {
   res.send("actualizando tareas");
 };
 
-const deleteTask = (req, res) => {
-  res.send("eliminando tareas");
+const deleteTask = async (req, res) => {
+  const [result] = await pool.query("DELETE FROM tasks WHERE id = ?", [
+    req.params.id,
+  ]);
+
+  if (result.affectedRows === 0)
+    return res.status(404).json({ message: "Task not found" });
+
+  return res.sendStatus(204);
 };
 
 module.exports = {
