12235fcf8381a3c0c430ddf7e204c601541c58eb
Rjchi <richardospina18@gmail.com>
2023-08-16 14:39:01 -0500

    listar tareas desde el front end

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,4 +1,7 @@
 import axios from "axios";
 
+export const getTaskRequest = async () =>
+    await axios.get("http://localhost:5000/tasks")
+
 export const createTaskRequest = async (task) =>
     await axios.post("http://localhost:5000/tasks", task);
--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import { getTaskRequest } from "../api/tasks.api";
+import TaskCard from "../components/TaskCard";
+
 const TaskPage = () => {
-  return <div>Task Page</div>;
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const loadTasks = async () => {
+      const response = await getTaskRequest();
+      setTasks(response.data);
+    };
+
+    loadTasks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      {
+        tasks.map((task) => (
+          <TaskCard key={task.id} task={task} />
+        ))
+      }
+    </div>
+  );
 };
 
 export default TaskPage;
