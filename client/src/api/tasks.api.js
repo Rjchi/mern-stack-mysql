a6cb6fc615486a37e13281930b1dfe2cb51d42c6
Rjchi <richardospina18@gmail.com>
2023-08-17 20:36:48 -0500

    crear variables de entorno

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,23 +1,24 @@
 import axios from "axios";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/tasks";
 
 // Plural
 export const getTaskRequest = async () =>
-  await axios.get("http://localhost:5000/tasks");
+  await axios.get(`${API_URL}`);
 
 export const createTaskRequest = async (task) =>
-  await axios.post("http://localhost:5000/tasks", task);
+  await axios.post(`${API_URL}`, task);
 
 export const deleteTaskRequest = async (id) =>
-  await axios.delete(`http://localhost:5000/tasks/${id}`);
+  await axios.delete(`${API_URL}/${id}`);
 
 // Singular
 export const getTaskReques = async (id) =>
-  await axios.get(`http://localhost:5000/tasks/${id}`);
+  await axios.get(`${API_URL}/${id}`);
 
 export const updateTaskRequest = async (id, newFields) =>
-  await axios.put(`http://localhost:5000/tasks/${id}`, newFields);
+  await axios.put(`${API_URL}/${id}`, newFields);
 
 export const toggleTaskDoneRequest = async (id, done) =>
-  await axios.put(`http://localhost:5000/tasks/${id}`, {
+  await axios.put(`${API_URL}/${id}`, {
     done: done,
   });
