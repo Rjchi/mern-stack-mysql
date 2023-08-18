fb81db179020d794e01054a08c2876334c2722f5
Rjchi <richardospina18@gmail.com>
2023-08-18 13:49:18 -0500

    modificar url de la API

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,24 +1,24 @@
 import axios from "axios";
-const API_URL = process.env.REACT_APP_API || "http://localhost:5000/tasks";
+// const API_URL = process.env.REACT_APP_API || "http://localhost:5000/tasks";
 
 // Plural
 export const getTaskRequest = async () =>
-  await axios.get(`${API_URL}`);
+  await axios.get(`/tasks`);
 
 export const createTaskRequest = async (task) =>
-  await axios.post(`${API_URL}`, task);
+  await axios.post(`/tasks`, task);
 
 export const deleteTaskRequest = async (id) =>
-  await axios.delete(`${API_URL}/${id}`);
+  await axios.delete(`/tasks/${id}`);
 
 // Singular
 export const getTaskReques = async (id) =>
-  await axios.get(`${API_URL}/${id}`);
+  await axios.get(`/tasks/${id}`);
 
 export const updateTaskRequest = async (id, newFields) =>
-  await axios.put(`${API_URL}/${id}`, newFields);
+  await axios.put(`/tasks/${id}`, newFields);
 
 export const toggleTaskDoneRequest = async (id, done) =>
-  await axios.put(`${API_URL}/${id}`, {
+  await axios.put(`/tasks/${id}`, {
     done: done,
   });
