25ced99381887fc784a6131507a1d812a6831dd9
Rjchi <richardospina18@gmail.com>
2023-08-17 22:31:57 -0500

    cambiar url de la API

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 // const API_URL = "http://localhost:5000";
-const API_URL = process.env.REACT_APP_API;
+// const API_URL = process.env.REACT_APP_API;
 
 // Plural
 export const getTaskRequest = async () =>
-  await axios.get(`${API_URL}`);
+  await axios.get("https://mysqlrn.netlify.app/tasks");
 
 export const createTaskRequest = async (task) =>
-  await axios.post(`${API_URL}`, task);
+  await axios.post("https://mysqlrn.netlify.app/tasks", task);
 
 export const deleteTaskRequest = async (id) =>
-  await axios.delete(`${API_URL}/${id}`);
+  await axios.delete(`https://mysqlrn.netlify.app/tasks/${id}`);
 
 // Singular
 export const getTaskReques = async (id) =>
-  await axios.get(`${API_URL}/${id}`);
+  await axios.get(`https://mysqlrn.netlify.app/tasks/${id}`);
 
 export const updateTaskRequest = async (id, newFields) =>
-  await axios.put(`${API_URL}/${id}`, newFields);
+  await axios.put(`https://mysqlrn.netlify.app/tasks/${id}`, newFields);
 
 export const toggleTaskDoneRequest = async (id, done) =>
-  await axios.put(`${API_URL}/${id}`, {
+  await axios.put(`https://mysqlrn.netlify.app/tasks/${id}`, {
     done: done,
   });
