6d62e161c25f1c0b37aad12920c639929007c9cc
Rjchi <richardospina18@gmail.com>
2023-08-17 22:26:55 -0500

    corregir url de la API

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -4,22 +4,22 @@ const API_URL = process.env.REACT_APP_API;
 
 // Plural
 export const getTaskRequest = async () =>
-  await axios.get(`${API_URL}/tasks`);
+  await axios.get(`${API_URL}`);
 
 export const createTaskRequest = async (task) =>
-  await axios.post(`${API_URL}/tasks`, task);
+  await axios.post(`${API_URL}`, task);
 
 export const deleteTaskRequest = async (id) =>
-  await axios.delete(`${API_URL}/tasks/${id}`);
+  await axios.delete(`${API_URL}/${id}`);
 
 // Singular
 export const getTaskReques = async (id) =>
-  await axios.get(`${API_URL}/tasks/${id}`);
+  await axios.get(`${API_URL}/${id}`);
 
 export const updateTaskRequest = async (id, newFields) =>
-  await axios.put(`${API_URL}/tasks/${id}`, newFields);
+  await axios.put(`${API_URL}/${id}`, newFields);
 
 export const toggleTaskDoneRequest = async (id, done) =>
-  await axios.put(`${API_URL}/tasks/${id}`, {
+  await axios.put(`${API_URL}/${id}`, {
     done: done,
   });
