96502356e541edbb747c9b9a99598b6d5d2a8ddb
Rjchi <richardospina18@gmail.com>
2023-08-17 21:57:49 -0500

    cambiar url de la API

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const API_URL = "https://mysqlrn.netlify.app";
+// const API_URL = "https://mysqlrn.netlify.app";
+const API_URL = process.env.REACT_APP_API;
 
 // Plural
 export const getTaskRequest = async () =>
@@ -19,6 +20,6 @@ export const updateTaskRequest = async (id, newFields) =>
   await axios.put(`${API_URL}/tasks/${id}`, newFields);
 
 export const toggleTaskDoneRequest = async (id, done) =>
-  await axios.put(`${API_URL}/tasks//${id}`, {
+  await axios.put(`${API_URL}/tasks/${id}`, {
     done: done,
   });
